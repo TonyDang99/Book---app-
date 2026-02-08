4f086039366bf867059ac2257f61a551195ce0a5
TonyDang99 <thanhtung220699@gmail.com>
2026-02-08 18:11:21 +0700

    Fix: Remove .git directory from mobile folder

---
+++ mobile/app/index.jsx
@@ -0,0 +1,22 @@
+import { Link } from "expo-router";
+import { StyleSheet,Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={styles.container }>
+      <Text style={styles.title}>Edit app/index.tsx to edit</Text>
+      <Link href="/signup">Signup Page</Link>
+      <Link href="/login">Login Page</Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title : { color: 'blue' }
+});
