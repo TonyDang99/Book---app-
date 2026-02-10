c3c88d6a443486423b93275729bcb62ab577fc83
TonyDang99 <thanhtung220699@gmail.com>
2026-02-10 11:02:54 +0700

    add useState

--- mobile/app/(auth)/index.jsx
+++ mobile/app/(auth)/index.jsx
@@ -1,8 +1,23 @@
- import { View, Text } from 'react-native'
- export default function Login() {
-   return (
-     <View>
-       <Text>Login Screen</Text>
-     </View>
-   )
- }
+import React, { useState } from "react";
+import { View, Text, Image } from "react-native";
+import styles from "../../assets/styles/login.styles";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleLogin = () => {};
+  return (
+    <View style={styles.container}>
+      <View style={styles.topIllustration}>
+        <Image
+        source={require("../../assets/images/i.png")}
+        style={styles.illustrationImage}
+        resizeMode="contain" 
+        />
+      </View>
+    </View>
+  );
+}
