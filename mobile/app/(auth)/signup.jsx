6d094ded88443e357640b92044a57ec9b68cd751
TonyDang99 <thanhtung220699@gmail.com>
2026-02-11 21:04:54 +0700

    Add TextInput

--- mobile/app/(auth)/signup.jsx
+++ mobile/app/(auth)/signup.jsx
@@ -1,4 +1,4 @@
-import { View, Text, Platform, KeyboardAvoidingView } from 'react-native';
+import { View, Text, Platform, KeyboardAvoidingView, TextInput } from 'react-native';
 import styles from "../../assets/styles/signup.styles";
 import { Ionicons } from "@expo/vector-icons";
 import COLORS from "../../constants/colors";
