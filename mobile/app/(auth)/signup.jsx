cb0b7a7f863694a9adcb3e9177b0ceb933c4c8b5
TonyDang99 <thanhtung220699@gmail.com>
2026-02-11 21:04:04 +0700

    add new username box

--- mobile/app/(auth)/signup.jsx
+++ mobile/app/(auth)/signup.jsx
@@ -1,7 +1,15 @@
 import { View, Text, Platform, KeyboardAvoidingView } from 'react-native';
 import styles from "../../assets/styles/signup.styles";
+import { Ionicons } from "@expo/vector-icons";
+import COLORS from "../../constants/colors";
+import { useState } from "react";
 
 export default function Signup() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [isLoading, setIsLoading] = useState(false); 
   return (
      <KeyboardAvoidingView
           style={{ flex: 1 }}
@@ -14,6 +22,30 @@ export default function Signup() {
             <Text style={styles.title}>BookWorm🐛</Text>
             <Text style={styles.subtitle}>Share your favorite reads</Text>
           </View>
+
+          <View style={styles.formContainer}>
+            {/* USERNAME INPUT */}
+            <View style={styles.inputGroup}>
+              <Text style={styles.label}>Username</Text>
+              <View style={styles.inputContainer}>
+                <Ionicons
+                  name="person-outline"
+                  size={20}
+                  color={COLORS.primary}
+                  style={styles.inputIcon}
+                />
+                <TextInput
+                  style={styles.input}
+                  placeholder="johndoe"
+                  placeholderTextColor={COLORS.placeholderText}
+                  value={username}
+                  onChangeText={setUsername}
+                  autoCapitalize="none"
+                />
+              </View>
+            </View>
+          </View>
+
               </View> 
               </View>
     </KeyboardAvoidingView>
