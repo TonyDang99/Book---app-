974fe4e3811dd0a99134c23f1a0223e7f342b45a
TonyDang99 <thanhtung220699@gmail.com>
2026-02-11 20:39:32 +0700

    update the login form

--- mobile/app/(auth)/index.jsx
+++ mobile/app/(auth)/index.jsx
@@ -21,44 +21,94 @@ export default function Login() {
   const [showPassword, setShowPassword] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
 
-  const handleLogin = () => {};
+  const handleLogin = () => { };
   return (
     <View style={styles.container}>
       {/* ILLUSTRATION */}
-        <View style={styles.topIllustration}>
-          <Image
-            source={require("../../assets/images/i.png")}
-            style={styles.illustrationImage}
-            resizeMode="contain"
-          />
-        </View>
+      <View style={styles.topIllustration}>
+        <Image
+          source={require("../../assets/images/i.png")}
+          style={styles.illustrationImage}
+          resizeMode="contain"
+        />
+      </View>
 
-      <View style = {styles.card}>
-        <View style = {styles.formContainer}>
+      <View style={styles.card}>
+        <View style={styles.formContainer}>
           {/* them vao day */}
-            {/* EMAIL */}
+          {/* EMAIL */}
+          <View style={styles.inputGroup}>
+            <Text style={styles.label}>Email</Text>
+            <View style={styles.inputContainer}>
+              <Ionicons
+                name="mail-outline"
+                size={20}
+                color={COLORS.primary}
+                style={styles.inputIcon}
+              />
+              <TextInput
+                style={styles.input}
+                placeholder="Enter your email"
+                placeholderTextColor={COLORS.placeholderText}
+                value={email}
+                onChangeText={setEmail}
+                keyboardType="email-address"
+                autoCapitalize="none"
+              />
+            </View>
+            {/* PASSWORD */}
             <View style={styles.inputGroup}>
-              <Text style={styles.label}>Email</Text>
+              <Text style={styles.label}>Password</Text>
               <View style={styles.inputContainer}>
                 <Ionicons
-                  name="mail-outline"
+                  name="lock-closed-outline"
                   size={20}
                   color={COLORS.primary}
                   style={styles.inputIcon}
                 />
+                {/* INPUT */}
                 <TextInput
                   style={styles.input}
-                  placeholder="Enter your email"
+                  placeholder="Enter your password"
                   placeholderTextColor={COLORS.placeholderText}
-                  value={email}
-                  onChangeText={setEmail}
-                  keyboardType="email-address"
-                  autoCapitalize="none"
+                  value={password}
+                  onChangeText={setPassword}
+                  secureTextEntry={!showPassword}
                 />
+
+                <TouchableOpacity
+                  onPress={() => setShowPassword(!showPassword)}
+                  style={styles.eyeIcon}
+                >
+                  <Ionicons
+                    name={showPassword ? "eye-outline" : "eye-off-outline"}
+                    size={20}
+                    color={COLORS.primary}
+                  />
+                </TouchableOpacity>
+
               </View>
             </View>
+            <TouchableOpacity style={styles.button} onPress={handleLogin} disabled={isLoading}>
+              {isLoading ? (
+                <ActivityIndicator color="#fff" />
+              ) : (
+                <Text style={styles.buttonText}>Login</Text>
+              )}
+            </TouchableOpacity>
+
+            {/* FOOTER */}
+            <View style={styles.footer}>
+              <Text style={styles.footerText}>Don't have an account?</Text>
+              <Link href="/signup" asChild>
+                <TouchableOpacity>
+                  <Text style={styles.link}>Sign Up</Text>
+                </TouchableOpacity>
+              </Link>
+            </View>
+          </View>
         </View>
-        </View>
-    </View>
-  );
+      </View>
+      </View>
+      );
 }
