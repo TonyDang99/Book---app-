b61e0d9c816dbc168a16559b6c05e1d85aac7e4b
TonyDang99 <thanhtung220699@gmail.com>
2026-05-29 21:13:32 +0700

    add cron

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";  
 import cor from "cors";
 import "dotenv/config";
+import job from "./lib/cron.js";
  
 import authRoutes from "./routes/authRoutes.js";
 import bookRoutes from "./routes/bookRoutes.js";
@@ -9,6 +10,9 @@ import {connectDB} from "./lib/db.js";
 const app = express();
 // nếu có PORT trong env thì dùng, ko thì dùng 3000 //
 const PORT = process.env.PORT || 3000;
+
+
+job.start(); // khởi động cron job  
 
 app.use(express.json());
 app.use(cor()); 
